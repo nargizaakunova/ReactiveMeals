276383fd11d9dfda9e3476bfd1c125bd4cd08cdf
Nargiza Akunova <nargizaakun@gmail.com>
2022-10-24 16:00:53 +0200

    feat: add MealItem, refactor code

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,10 @@
+import classes from './MealItemForm.module.css';
+
+const MealItemForm = () => {
+  return (
+    <form className={classes.form}>
+      <button>Add to card</button>
+    </form>
+  );
+};
+export default MealItemForm;
