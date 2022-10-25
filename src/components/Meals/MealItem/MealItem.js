8aede953eeaceb70346eb4162297b246be0a5037
Nargiza Akunova <nargizaakun@gmail.com>
2022-10-25 14:17:36 +0200

    feat: add Input component and form, fix unique id

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
-import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
+
+import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -11,7 +12,7 @@ const MealItem = (props) => {
         <p className={classes.price}>{price}</p>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
