8333794d1cd26ba181c8a72ffc26ed3c985d043c
Nargiza Akunova <nargizaakun@gmail.com>
2022-10-25 21:44:23 +0200

    feat: close when click on close button on modal and click on backdrop

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>ReactiveMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
         <img src={meals} alt="A table full of delicious food" />
