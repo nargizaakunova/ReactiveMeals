6c4405b24d4f1bcb4e1f6151c667a9456ac66d1b
Nargiza Akunova <nargizaakun@gmail.com>
2022-10-29 22:32:16 +0200

    feat: my version add cart reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import { useContext } from 'react';
+import CartContext from '../../store/context-cart';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,11 +1,34 @@
+import { useReducer } from 'react';
 import CartContext from './context-cart';
 
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    return {
+      items: [...state.items, action.item],
+      totalAmount: state.totalAmount + action.item.amount * action.item.price,
+    };
+  }
+  if (action.type === 'REMOVE') {
+    return {};
+  }
+  return { items: state.items, totalAmount: state.totalAmount };
+};
+const defaultCartState = { items: [], totalAmount: [] };
 const CartProvider = (props) => {
-  const addItemToCardHandler = (item) => {};
-  const removeItemFromCardHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCardHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', item: item });
+  };
+  const removeItemFromCardHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCardHandler,
     removeItem: removeItemFromCardHandler,
   };
